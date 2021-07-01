817c5441a02e2166975f22e210302d96c89953c1
Yohali20 <yojanqz10@gmail.com>
2021-07-01 11:14:39 -0500

    miprimercommit

---
+++ index.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilo.css">
+    
+    <title>Productos!</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+          <div class="col">
+            <nav class="navbar navbar-expand-lg fixed-top">
+              <a href="index.html">
+                <img src="img/logo4.png" alt="" width="200px">
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <img src="img/menu.png" alt="">
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#" id="conocenos">Conocenos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="galeria.html" id="producto">Productos</a>
+                  </li>
+                  <li class="nav-item me-2">
+                    <a class="nav-link" href="./cuenta.php" class="btn" id="cuenta">Mi cuenta</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#" id="carritos">
+                      <img src="img/carrito1.png" alt="" width="20px">
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <div class="row mt-5">
+              <div class="col mt-5 text-center" style="color: rgb(150, 150, 150);">
+                <p>Entrega a partir de una compra minima de $1000.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <section>
+        <div class="container tarjeta">
+          <h3 class="text-center vendido">Catálogo de productos</h3>
+    </section>
+
+      <footer>
+        <div class="container mt-5">
+          <div class="row">
+            <div class="col social-media">
+              <a href="" class="social-media-icon">
+                <img src="img/face.png" alt="" width="100%">
+              </a>
+              <a href="" class="social-media-icon">
+                <img src="img/insta.png" alt="" width="100%">
+              </a>
+              <a href="" class="social-media-icon">
+                <img src="img/pin.png" alt="" width="100%">
+              </a>
+            </div>
+          </div>
+          <div class="row ">
+            <div class="col footer-content">
+              <a href="" class="texto">
+                <p>Preguntas frecuentas</p>
+              </a>
+              <a href="#" class="texto">
+                <p>Envíos</p>
+              </a>
+              <a href="" class="texto">
+                <p>Terminos y condiciones</p>
+              </a>
+              <a href="" class="texto">
+                <p>Aviso de provacidad</p>
+              </a>
+              <a href="" class="texto">
+                <p>Terminos del servicio</p>
+              </a>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col tarjeta-pago">
+              <img src="img/master.png" alt="" class="pago">
+              <img src="img/visa.png" alt="" class="pago">
+              <img src="img/oxxo.png" alt="" class="pago">
+            </div>
+          </div>
+          <div class="col text-center">
+            <p style="padding-bottom: 20px;"><small>© 2021 EcoFemme</small></p>
+          </div>
+        </div>
+      </footer>
+    
+      <!-- CARD2 -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
